917d20b643f46a598fe60ccefa86489632267678
RiA <105122539+glorianwachukwu1@users.noreply.github.com>
2023-09-18 23:47:42 -0400

    Update Fall2023_Lab1.R

--- Fall2023_Lab1.R
+++ Fall2023_Lab1.R
@@ -4,3 +4,30 @@ library(readxl)
 EPI_data<-read.csv("~/DATA ANALYTICS/LAB/EPI/2010EPI_data.csv")
 
 View(EPI_data)
+
+# I noticed that the heading of the table is showing x x x , so the code below will fix it
+
+names(EPI_data)<-as.matrix(EPI_data[1,])
+EPI_data <- EPI_data[-1,]
+EPI_data[]<- lapply(EPI_data, function(x) type.convert(as.character(x)))
+EPI_data
+
+
+#To view table and check if the heading has been fixed
+View(EPI_data)
+
+#sets the 'default' object
+attach(EPI_data)
+
+
+# launches a simple data editor – test it!
+fix(EPI_data)
+
+
+
+
+
+
+
+
+
