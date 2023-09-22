745534407c9407cb899eb87155e6cb0debaefe3d
glorianwachukwu1 <nwachukwuogochukwu@gmail.com>
2023-09-22 04:20:08 -0400

    Update LAB_2.R

--- LAB_2.R
+++ LAB_2.R
@@ -1,1 +1,8 @@
+EPI_data<-read.csv("~/DATA ANALYTICS/LAB/EPI/2010EPI_data.csv")
+
+View(EPI_data)
+
+# changing the heading from x x x, to its original name
+names(EPI_data)<-as.matrix(EPI_data[1,])
+EPI_data <-EPI_data[-1,]
 
