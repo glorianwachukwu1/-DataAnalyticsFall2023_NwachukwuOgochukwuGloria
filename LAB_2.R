58e0e85a8c655a974393287dcd76dd338feacb74
glorianwachukwu1 <nwachukwuogochukwu@gmail.com>
2023-09-24 18:41:34 -0400

    Update LAB_2.R

--- LAB_2.R
+++ LAB_2.R
@@ -5,4 +5,11 @@ View(EPI_data)
 # changing the heading from x x x, to its original name
 names(EPI_data)<-as.matrix(EPI_data[1,])
 EPI_data <-EPI_data[-1,]
+EPI_data[]<- lapply(EPI_data, function(x) type.convert(as.character(x)))
+EPI_data
 
+#Cumulative density function
+
+plot(ecdf(EPI),do.points=FALSE,verticals=TRUE)
+
+#Quantile-Quantile
