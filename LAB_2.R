6823e2bd5b6a7eb836f10a0101040b383d5ae148
glorianwachukwu1 <nwachukwuogochukwu@gmail.com>
2023-09-26 02:36:54 -0400

    Update LAB_2.R

--- LAB_2.R
+++ LAB_2.R
@@ -1,15 +1,1 @@
-EPI_data<-read.csv("~/DATA ANALYTICS/LAB/EPI/2010EPI_data.csv")
-
-View(EPI_data)
-
-# changing the heading from x x x, to its original name
-names(EPI_data)<-as.matrix(EPI_data[1,])
-EPI_data <-EPI_data[-1,]
-EPI_data[]<- lapply(EPI_data, function(x) type.convert(as.character(x)))
-EPI_data
-
-#Cumulative density function
-
-plot(ecdf(EPI),do.points=FALSE,verticals=TRUE)
-
-#Quantile-Quantile
+#Linear basis and least-squares constraints
